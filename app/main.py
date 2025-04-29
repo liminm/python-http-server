64dc5aa7c2b2de3ebbf8cb68e24c9203b644e855
liminm <liminmalek@gmail.com>
2025-04-29 17:14:52 +0200

    Comment out file path construction for HTTP file requests

--- app/main.py
+++ app/main.py
@@ -91,7 +91,7 @@ def route_request(parsed_request):
         elif path.startswith("/files/"):
             # Example: /files/somefile.txt
             file_path = path[len("/files/"):]
-            file_path = "./tmp/" + file_path
+            # file_path = "./tmp/" + file_path
             print(f"Path: {path}")
             print(f"File path requested: {file_path}")
             try:
