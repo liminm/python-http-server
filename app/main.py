b0c3710dbf11ee79df8dc73318b5e83dec4332b4
liminm <liminmalek@gmail.com>
2025-04-29 16:13:55 +0200

    Add support for echoing content in HTTP GET requests

--- app/main.py
+++ app/main.py
@@ -20,9 +20,18 @@ def main():
                 reqMsg = req.decode(errors="replace").split(" ")
                 reqMethod = reqMsg[0]
                 reqPath = reqMsg[1]
+                reqVersion = reqMsg[2]
                 print(f"{req2=}")
                 if req2.startswith("GET / HTTP/1.1"):
                     response = "HTTP/1.1 200 OK\r\n\r\n"
+                # elif req2.startswith("GET /echo/") and reqVersion == "HTTP/1.1":
+                elif req2.startswith("GET /echo/"):
+
+                    echo_content = reqPath.split("/")[-1]
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n{echo_content}"
+                    print(f"{response=}")
+
+
                 else:
                     response = "HTTP/1.1 404 Not Found\r\n\r\n"
                 conn.send(response.encode())
