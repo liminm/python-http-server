5d5222c096f2c5b803a571e759bf4c4ab7a4f555
liminm <liminmalek@gmail.com>
2025-04-29 15:59:12 +0200

    Implement basic HTTP server with GET request handling

--- app/main.py
+++ app/main.py
@@ -8,7 +8,32 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    # server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")  # wait for client
+    try:
+        while True:
+            try:
+                conn, addr = server_socket.accept()
+                print(f"Connection from {addr}")
+
+                req = conn.recv(1024)
+                req2 = req.decode()
+                reqMsg = req.decode(errors="replace").split(" ")
+                reqMethod = reqMsg[0]
+                reqPath = reqMsg[1]
+                print(f"{req2=}")
+                if req2.startswith("GET / HTTP/1.1"):
+                    response = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+                conn.send(response.encode())
+                conn.shutdown(socket.SHUT_WR)
+                conn.close()
+            except Exception as e:
+                print(f"Error handling connection: {e}")
+    except KeyboardInterrupt:
+        print("\nServer interrupted. Shutting down.")
+    finally:
+        server_socket.close()
 
 
 if __name__ == "__main__":
